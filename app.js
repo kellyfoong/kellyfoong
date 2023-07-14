d2eeca12e524e4ff0cebf6a80c1368ae1e3e3777
kellyfoongly <heyyitskellyy@gmail.com>
2023-07-14 20:48:02 +0800

    Update Sky Color

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-		const ambient = new THREE.HemisphereLight(0xFFFFFF, 0xAAAAAA, 0.8);
+		const ambient = new THREE.HemisphereLight(0xee5d6c, 0xeeaf61, 1.5);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
